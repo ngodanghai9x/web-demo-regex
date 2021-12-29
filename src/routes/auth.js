05b5cf04855ff8cc8542f68e9887b22c5260eb67
Lam <lam@mission.plus>
2021-12-29 21:59:52 +0700

    pending forgetPassword

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,21 +2,24 @@ import express from 'express';
 import { renderLogin, postLogin, renderRegister, postRegister } from 'controller/auth.js';
 import { validateRegister, validateLogin } from '../middlewares/validateRegister';
 import { requireAuth } from '../middlewares/requireAuth';
-import { postChangePassword, renderChangePassword } from '../controller/auth';
+import { postChangePassword, renderChangePassword, renderForgetPassword } from '../controller/auth';
 
 const router = express.Router();
 // /auth
 router.get('/login', renderLogin);
 router.post('/login', validateLogin, postLogin);
 
-router.get('/logout', (req,res) => {
+router.get('/logout', (req, res) => {
 
 });
 
 router.get('/register', renderRegister);
-router.post('/register',validateRegister, postRegister);
+router.post('/register', validateRegister, postRegister);
 
 router.get('/change-password', requireAuth, renderChangePassword);
-router.post('/change-password',requireAuth, postChangePassword);
+router.post('/change-password', requireAuth, postChangePassword);
+
+router.get('/forget-password', renderForgetPassword);
+router.post('/forget-password', postChangePassword);
 
 export default router;
